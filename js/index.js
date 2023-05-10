0a053d0d60a54223ba631fbcf1a5d9fdeefca584
juan allasia <juanmanuel-a@hotmail.com.ar>
2023-05-10 12:07:21 -0300

    arreglos dos

--- js/index.js
+++ js/index.js
@@ -6,16 +6,34 @@ console.log("", saludar);
 const form = document.getElementById("formulario");
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault(); 
+  event.preventDefault(); 
 
   const nombre = document.getElementById("nombre").value;
   const contraseña = document.getElementById("contraseña").value;
   const fecha_nacimiento = document.getElementById("fecha_nacimiento").value;
 
-
+  
   sessionStorage.setItem("nombre", nombre);
   sessionStorage.setItem("contraseña", contraseña);
   sessionStorage.setItem("fecha_nacimiento", fecha_nacimiento);
 
-  window.location.href = "bienvenida.html"
+
 });
+
+
+
+function checkEdad() {
+  var fechaNacimiento = document.getElementById("fecha_nacimiento").value;
+  var fechaActual = new Date();
+  var fechaNacimientoDate = new Date(fechaNacimiento);
+  var edad = fechaActual.getFullYear() - fechaNacimientoDate.getFullYear();
+  var mes = fechaActual.getMonth() - fechaNacimientoDate.getMonth();
+  if (mes < 0 || (mes === 0 && fechaActual.getDate() < fechaNacimientoDate.getDate())) {
+    edad--;
+  }
+  if (edad < 18) {
+    document.getElementById("resultado").innerHTML = "Lo siento, debes ser mayor de 18 años para acceder a esta página.";
+  } else {
+    window.location.href = "bienvenida.html";
+  }
+}
